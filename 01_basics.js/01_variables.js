cf957bbbd0abe27c75e6bf687568e3d6f98735c6
samarthpandey786 <92950334+samarthpandey786@users.noreply.github.com>
2025-03-24 06:33:37 +0000

    datatypes

---
+++ 01_basics.js/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 133232
+
+let accoutnEmail = "samarthpandey76@gmail.com" 
+var accountPassword = "1234"
+accoutnCity = "ghaziabad"// kar sakte he
+let accoutState ;
+
+// accountId = 2 // not allowed 
+
+accoutnEmail = "sama";
+accountPassword = '1212';
+accoutnCity = "delhi";
+console.log(accountId);
+
+console.table([accountId , accountPassword , accoutnEmail , accoutnCity , accoutState]);
+
+/* 
+perfer not to use var 
+because of issue in block scope and functional scopt:
+*/
---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" // treate all JS code as newer verison 
+
+// alert(2 + 4); // er are using nodejs ,not browser
+
+let name = "samarth pandey";
+let age = 23;
+let isloggedIN = true;
+let temp = null;
+
+// number => 2 ^ 53 the number you can add
+// bigInt => 
+//string => ""
+//boolean => true or false
+// null => standalone value represent null or empty value
+// undefined => not assigned the value yet
+// sysbol => for the uniqueness using symbol
+
+//object 
+
+console.log(typeof null );
