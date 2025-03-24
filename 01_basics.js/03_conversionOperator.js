542e44c63bd1e9acac85730bfc638b547e2a7dbb
samarthpandey786 <92950334+samarthpandey786@users.noreply.github.com>
2025-03-24 07:00:55 +0000

    convertion of datatypes

---
+++ 01_basics.js/03_conversionOperator.js
@@ -0,0 +1,37 @@
+let score = "33";
+let score2 = 'a33'
+let score3 = null;
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number//convertion operator 
+// (score)
+
+// let valueInNum = Number(score2);
+// console.log(typeof valueInNum);
+// console.log(valueInNum);// show's NaN mean not a number so better check the number first
+
+// console.log(typeof valueInNumber);
+
+// in value converion like number if the value if pure number it will convert it into the number
+
+// but if it contain the any string value it also convert it into the number so while doing this better check the value first:
+
+
+//Notes
+
+//"33" => 33 number 
+// "33abc" => NaN type is number confusing
+// true => 1 : false => 0
+
+// 1 => true 
+// 0 => false
+// '' => flase
+// 'sam' => true
+
+
+let num = 33;
+let stringNum = String(num);
+console.log(typeof stringNum);
+console.log(stringNum)
