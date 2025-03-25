f16c46afd9c38ebd966871449ce21eed51067e85
samarthpandey786 <92950334+samarthpandey786@users.noreply.github.com>
2025-03-25 05:56:20 +0000

    stack and heap

---
+++ 01_basics.js/04_stack-heap.js
@@ -0,0 +1,24 @@
+//stack(Primitive) , Heap(non - primitive)
+
+let myname = "samarthpandey";// work on stack
+
+let myname2 = myname;
+myname2 = "kannu";
+
+console.log(myname2);
+console.log(myname);
+
+let user1 = {
+    emial:"samarthpandey@",// work on heap
+    upi: 9311250731
+}
+
+let user2 = user1
+
+user2.emial = "samathpandey2@"
+console.log(user1);
+console.log(user2);
+
+// in stack we got copy of orignal value changes in the copy does not effect the orignal value
+
+// in Heap we go the refrence of the orignal value which mean changes in the refrenced variable can change the value of origanl value 
