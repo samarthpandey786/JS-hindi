f4c39759aa2b5a7b88efe7c8834cb315267da1ee
samarthpandey786 <92950334+samarthpandey786@users.noreply.github.com>
2025-03-25 10:12:10 +0000

    array2 karne he kal okk

---
+++ 02_basics/02_array.js
@@ -0,0 +1,11 @@
+const marvel = ["Hulk" , 
+    "thor" , "spiderman"
+]
+
+const dc = ['supermam' , 'flash' , 
+    'batman'
+]
+
+marvel.push(dc);// problem array inside array:
+console.log(marvel)
+
