8ae0e2645d19b30d67da251a7d67f971440b0cfc
samarthpandey786 <92950334+samarthpandey786@users.noreply.github.com>
2025-03-25 08:01:32 +0000

    nums and math

---
+++ 01_basics.js/06_nums_maths.js
@@ -0,0 +1,35 @@
+// const score = 400
+
+// const bal = new Number(300)// this is an obeject new
+// console.log(score);
+// console.log(bal);
+
+// console.log(bal.toString().length)// to string method can change the number into string :
+
+// console.log(bal.toFixed(2)); // help us to set the how much floting value i want like 100.22 :
+
+// const num = 123.3244
+
+// console.log( num.toPrecision(3));// to make the precise value of the figure u give
+
+// const hund = 1000000
+
+// console.log(hund.toLocaleString('en-IN'))// can implement comas in the large degit number by defalut in the us standards but you can change to indian 
+
+// Maths::
+
+console.log(Math);
+
+console.log(Math.abs(-4));// only -ev value are change to positive  absolute
+
+
+console.log(Math.round(4.6))// round off figure it gave
+
+console.log(Math.ceil(4.4))// takes a upper half value and make the round off instantly to next:
+
+console.log(Math.floor(4.6))// take lower half values as round figure
+
+
+
+
+
