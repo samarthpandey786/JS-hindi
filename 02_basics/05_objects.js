9909b06128b826345a8c63a765d293b29764083e
samarthpandey786 <92950334+samarthpandey786@users.noreply.github.com>
2025-03-27 07:25:07 +0000

    object and json talk

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,34 @@
+// de-structuring and json API
+ const  cours = {
+    cours_name : 'JS',
+    price : "999",
+    course_admin : "samarth"
+
+ }
+
+//  syntax to acces the memeber of the object another mehtod
+
+const{course_admin : admin} = cours// more clean way to access it we can de-struct mens we can change the key name as well  using (:) <= this
+
+// de -structuring object : {}
+
+console.log(admin);
+
+// API 
+
+// json = In json the key also comes in string and the value also :
+// {
+//     'name' : "samrth pandey",
+//     'course' :  "BCA",
+//     'price' : "free"
+
+// }
+
+// API also get in a array 
+[// array 
+    {}// object
+]
+
+
+
+
